fb0d7cc772c3469985f9cb6b2c1acf012fcba96f
Justin Mucelli <10000turtles@gmail.com>
2023-03-14 17:49:14 -0400

    test

--- Project/ProjPart1-Circles.py
+++ Project/ProjPart1-Circles.py
@@ -23,19 +23,38 @@ from pyomo.opt import SolverFactory
 import pyomo
 
 from pyomo.environ import *
+from graphics import *
+
+circRad = 1
+circles = 6
 
 model = ConcreteModel()
 
-model.x = Var(bounds=(1.0,10.0),initialize=5.0)
-model.y = Var(within=Binary)
+model.xyBound = Var(within=NonNegativeReals)
 
-model.c1 = Constraint(expr=(model.x-4.0)**2 - model.x <= 50.0*(1-model.y))
-model.c2 = Constraint(expr=model.x*log(model.x)+5.0 <= 50.0*(model.y))
+for i in range(circles):
+    model.add_component('x'+str(i),Var(within = NonNegativeReals,initialize=i))
+    model.add_component('y'+str(i),Var(within = NonNegativeReals,initialize=i+1))
 
-model.objective = Objective(expr=model.x, sense=minimize)
+    model.add_component('Uboundx'+str(i), Constraint(expr=model.component('x'+str(i)) + circRad <= model.xyBound))
+    model.add_component('Uboundy'+str(i), Constraint(expr=model.component('y'+str(i)) + circRad <= model.xyBound))
 
-SolverFactory('mindtpy').solve(model, mip_solver='glpk', nlp_solver='ipopt', tee=True) 
+    model.add_component('Lboundx'+str(i), Constraint(expr=model.component('x'+str(i)) >= circRad ))
+    model.add_component('Lboundy'+str(i), Constraint(expr=model.component('y'+str(i)) >= circRad ))
+
+for i in range(circles):
+    for j in range(i+1,circles,1):
+        model.add_component('dist'+str(i)+str(j),Constraint(expr=
+        (model.component('x'+str(i)) - model.component('x'+str(j)))**2 + 
+        (model.component('y'+str(i)) - model.component('y'+str(j)))**2 >= (2*circRad)**2))
+
+model.objective = Objective(expr=model.xyBound, sense=minimize)
+
+opt = SolverFactory('mindtpy')
+opt.solve(model, mip_solver='glpk', nlp_solver='ipopt') 
 
 model.objective.display()
-model.display()
-model.pprint()
+# model.display()
+# model.pprint()
+
+win = GraphWin()
